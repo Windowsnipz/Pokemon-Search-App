9722a903d37ede52de0b151c2178f5f9da6cad14
windowsnipz <nstrickland2014@gmail.com>
2024-06-09 14:52:20 -0400

    javascript progress

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const input = document.getElementById('search-input');
 const searchBtn = document.getElementById('search-button');
+
+
+searchBtn.addEventListener('click', () => {
+    const query = input.value.toLowerCase();
+    fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${query}`)
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+        })
+});
